2e61d32878ce11124fe8a54ab7cd9764176149c7
Danilo Jr Dela Cruz <delacruz.danilojr@gmail.com>
2024-01-31 17:09:28 +0000

    Add utility code to create non-overlapping df

--- resources/utilities/utils.py
+++ resources/utilities/utils.py
@@ -61,3 +61,48 @@ def plot_spectrogram(spectrogram_path: str):
         ax.set_yticklabels(frequencies[::label_interval])
     plt.tight_layout()
     plt.show()
+
+
+def generate_non_overlapping_data(df):
+    """eeg_df is train.csv read in.
+    This modifies the train_csv (metadata) so that we only have one segment per eeg_id.
+    https://www.kaggle.com/code/yorkyong/exploring-eeg-a-beginner-s-guide
+    """
+    TARGETS = df.columns[-6:]
+    # Creating a Unique EEG Segment per eeg_id:
+    # The code groups (groupby) the EEG data (df) by eeg_id. Each eeg_id represents a different EEG recording.
+    # It then picks the first spectrogram_id and the earliest (min) spectrogram_label_offset_seconds for each eeg_id. This helps in identifying the starting point of each EEG segment.
+    # The resulting DataFrame train has columns spec_id (first spectrogram_id) and min (earliest spectrogram_label_offset_seconds).
+    train = df.groupby('eeg_id')[['spectrogram_id','spectrogram_label_offset_seconds']].agg(
+        {'spectrogram_id':'first','spectrogram_label_offset_seconds':'min'})
+    train.columns = ['spec_id','min']
+
+
+    # Finding the Latest Point in Each EEG Segment:
+    # The code again groups the data by eeg_id and finds the latest (max) spectrogram_label_offset_seconds for each segment.
+    # This max value is added to the train DataFrame, representing the end point of each EEG segment.
+    tmp = df.groupby('eeg_id')[['spectrogram_id','spectrogram_label_offset_seconds']].agg(
+        {'spectrogram_label_offset_seconds':'max'})
+    train['max'] = tmp
+
+
+    tmp = df.groupby('eeg_id')[['patient_id']].agg('first') # The code adds the patient_id for each eeg_id to the train DataFrame. This links each EEG segment to a specific patient.
+    train['patient_id'] = tmp
+
+
+    tmp = df.groupby('eeg_id')[TARGETS].agg('sum') # The code sums up the target variable counts (like votes for seizure, LPD, etc.) for each eeg_id.
+    for t in TARGETS:
+        train[t] = tmp[t].values
+
+    y_data = train[TARGETS].values # It then normalizes these counts so that they sum up to 1. This step converts the counts into probabilities, which is a common practice in classification tasks.
+    y_data = y_data / y_data.sum(axis=1,keepdims=True)
+    train[TARGETS] = y_data
+
+    tmp = df.groupby('eeg_id')[['expert_consensus']].agg('first') # For each eeg_id, the code includes the expert_consensus on the EEG segment's classification.
+    train['target'] = tmp
+
+    train = train.reset_index() # This makes eeg_id a regular column, making the DataFrame easier to work with.
+    print('Train non-overlapp eeg_id shape:', train.shape )
+    train.head()
+
+    return train
