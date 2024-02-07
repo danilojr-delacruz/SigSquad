f689a2d178be39188e36da6e390cb0a90cc5b50e
Danilo Jr Dela Cruz <delacruz.danilojr@gmail.com>
2024-02-07 09:53:09 +0000

    Rewrite DataGenerator to use PyTorch Dataset.

--- models/EfficientNetB0Starter/input_utils.py
+++ models/EfficientNetB0Starter/input_utils.py
@@ -42,69 +42,73 @@ def create_modified_eeg_metadata_df(eeg_metadata_df):
     return train
 
 
-# TODO: Migrate onto PyTorch Lightning
-# Train Dataloader
-class DataGenerator(tf.keras.utils.Sequence):
+# Refer to this to understand Dataset
+# https://pytorch.org/tutorials/beginner/basics/data_tutorial.html
+class DataGenerator(Dataset):
     "Generates data for Keras"
-    def __init__(self, data, specs, eeg_specs, TARGETS,
+    def __init__(self, data, specs, eeg_specs, transform=None,
                  batch_size=32, shuffle=False, augment=False, mode="train"):
 
+        # Core data
         self.data = data
+        self.specs = specs
+        self.eeg_specs = eeg_specs
+
         self.batch_size = batch_size
         self.shuffle = shuffle
         # TODO: Not implemented yet, will need albumentations
         self.augment = augment
+        # TODO: Do we need to worry about the mode? Doesn't PL handle that?
         self.mode = mode
-        self.specs = specs
-        self.eeg_specs = eeg_specs
-        self.on_epoch_end()
 
+        self.transform = transform
+
+        # TODO: This can be inferred from data!
         self.TARGETS = TARGETS
+        self.num_targets = len(TARGETS)
+
+        self.img_width  = 128
+        self.img_height = 256
+
+        # There are 4 types: Left and Right for Parasagittal and Lateral
+        # Then 8 in total because using Kaggle Spectrograms and one inferred from EEG
+        self.num_spectrograms = 8
 
     def __len__(self):
-        "Denotes the number of batches per epoch"
-        ct = int( np.ceil( len(self.data) / self.batch_size ) )
-        return ct
+        return len(self.data)
 
-    def __getitem__(self, index):
-        "Generate one batch of data"
-        indexes = self.indexes[index*self.batch_size:(index+1)*self.batch_size]
-        X, y = self.__data_generation(indexes)
-        return X, y
+    def transform(self, img):
+        # LOG TRANSFORM SPECTROGRAM
+        img = np.clip(img,np.exp(-4),np.exp(8))
+        img = np.log(img)
 
-    def on_epoch_end(self):
-        "Updates indexes after each epoch"
-        self.indexes = np.arange( len(self.data) )
-        if self.shuffle: np.random.shuffle(self.indexes)
+        # STANDARDIZE PER IMAGE
+        ep = 1e-6
+        m = np.nanmean(img.flatten())
+        s = np.nanstd(img.flatten())
+        img = (img-m)/(s+ep)
+        img = np.nan_to_num(img, nan=0.0)
 
-    def __data_generation(self, indexes):
+        return img
+
+    def __getitem__(self, idx):
         "Generates data containing batch_size samples"
 
-        X = np.zeros((len(indexes),128,256,8),dtype="float32")
-        y = np.zeros((len(indexes),6),dtype="float32")
-        img = np.ones((128,256),dtype="float32")
+        # idx will contain the indices for our batch
+        # Shape is (batch_size, )
+        X = np.zeros((self.batch_size, self.img_width, self.img_height, self.num_spectrograms), dtype="float32")
+        y = np.zeros((self.batch_size, self.num_targets), dtype="float32")
+        img = np.ones((self.img_width, self.img_height),dtype="float32")
 
-        for j,i in enumerate(indexes):
+        for j,i in enumerate(idx):
             row = self.data.iloc[i]
-            if self.mode=="test":
-                r = 0
-            else:
-                r = int( (row["min"] + row["max"])//4 )
+            # if self.mode=="test":
+            #     r = 0
+            r = int( (row["min"] + row["max"])//4 )
 
             for k in range(4):
                 # EXTRACT 300 ROWS OF SPECTROGRAM
-                img = self.specs[row.spec_id][r:r+300,k*100:(k+1)*100].T
-
-                # LOG TRANSFORM SPECTROGRAM
-                img = np.clip(img,np.exp(-4),np.exp(8))
-                img = np.log(img)
-
-                # STANDARDIZE PER IMAGE
-                ep = 1e-6
-                m = np.nanmean(img.flatten())
-                s = np.nanstd(img.flatten())
-                img = (img-m)/(s+ep)
-                img = np.nan_to_num(img, nan=0.0)
+                img = self.transform(self.specs[row.spec_id][r:r+300,k*100:(k+1)*100].T)
 
                 # CROP TO 256 TIME STEPS
                 X[j,14:-14,:,k] = img[:,22:-22] / 2.0
