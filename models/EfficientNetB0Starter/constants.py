06c1e0d4c4ed9a418ff80bb34ace9de40796da0b
Danilo Jr Dela Cruz <delacruz.danilojr@gmail.com>
2024-02-20 09:51:27 +0000

    Move LABEL-ID stuff to constants.

--- models/EfficientNetB0Starter/constants.py
+++ models/EfficientNetB0Starter/constants.py
@@ -2,3 +2,10 @@ TARGETS = [
     "seizure_vote",	"lpd_vote", "gpd_vote",
     "lrda_vote", "grda_vote", "other_vote"
     ]
+
+# This gives the mapping between labels and their class id
+LABEL_TO_ID = {
+    "Seizure":0, "LPD" :1, "GPD"  :2,
+    "LRDA"   :3, "GRDA":4, "Other":5
+}
+ID_TO_LABEL = {x:y for y,x in LABEL_TO_ID.items()}
