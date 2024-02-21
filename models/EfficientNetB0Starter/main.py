3fc7f798aee5190f8960f879c48425b369b95d87
Danilo Jr Dela Cruz <delacruz.danilojr@gmail.com>
2024-02-21 15:57:40 +0000

    Remove model training. That happens offline.

--- models/EfficientNetB0Starter/main.py
+++ models/EfficientNetB0Starter/main.py
@@ -7,6 +7,7 @@ TEST_METADATA_DIR    = "/kaggle/input/hms-harmful-brain-activity-classification/
 TEST_SPECTROGRAM_DIR = "/kaggle/input/hms-harmful-brain-activity-classification/test_spectrograms"
 TEST_EEG_DIR         = "/kaggle/input/hms-harmful-brain-activity-classification/test_eegs"
 
+MODEL_DIR            = "/kaggle/input/efficientnetb0-19-02-2024/trained_model_19-02-2024.pt"
 
 
 import pandas as pd
@@ -19,24 +20,12 @@ from input_utils import modify_train_metadata, PreloadedSpectrogramFetcher, \
 from model import LitENB0
 from constants import TARGETS
 
-# 1. Reading Data --------------------------------------------------------------
-# Ignore the sub_ids
-train_metadata = pd.read_csv(TRAIN_METADATA_DIR)
-train_metadata = modify_train_metadata(train_metadata)
-
-# Read precomputed spectrograms (from kaggle and eeg)
-# that are all placed in one dictionary
-spectrograms     = np.load(KAGGLE_SPECTROGRAM_DIR, allow_pickle=True).item()
-eeg_spectrograms = np.load(EEG_SPECTROGRAM_DIR,allow_pickle=True).item()
-
-# 2. Build and Train Model -----------------------------------------------------
+# 1. Build and Load Model ------------------------------------------------------
 
 model = LitENB0()
-model.load_state_dict(torch.load("../../personal_sandbox/trained_model_19-02-2024.pt"))
+model.load_state_dict(torch.load(MODEL_DIR))
 
-del eeg_spectrograms, spectrograms; gc.collect()
-
-# 3. Test Model ---------------------------------------------------------------
+# 2. Generate Predictions ------------------------------------------------------
 
 # Put into evaluation mode, turns off dropout
 model.eval()
@@ -49,14 +38,14 @@ test_spectrograms     = preload_spectrograms(TEST_SPECTROGRAM_DIR)
 # If so, essentially left hand side is full spectrogram, and right hand side is zoomed in
 test_eeg_spectrograms = preload_eeg_spectrograms(test_metadata, TEST_EEG_DIR)
 
-# READ ALL EEG SPECTROGRAMS AND CONVERT
-
 fetcher      = PreloadedSpectrogramFetcher(test_spectrograms, test_eeg_spectrograms)
 test_dataset = TestDataset(test_metadata, fetcher, shuffle=False)
 # TODO: Can speed up evaluations with GPUs later
 test_loader  = DataLoader(test_dataset)
 
 predictions = model.predict(test_loader)
+
+# 3. Create Submission ---------------------------------------------------------
 
 # Ensure predictions sum to 1
 predictions /= predictions.sum(dim=1).unsqueeze(1)
